28935acc15721eb3f212f6225bbf0d339ee39da6
yiren <irenew713@gmail.com>
2017-02-10 17:44:39 -0600

    support tune gamma

--- cls/svm/tune_params_svm.py
+++ cls/svm/tune_params_svm.py
@@ -12,9 +12,10 @@ import subprocess
 
 from subprocess import STDOUT
 
-kernels = [0, 1, 2] 
+kernels = [0, 2]
 kernel_name = ['linear', 'polynomial', 'rbf', 'sigmoid']
 Cparas = [2**i for i in range(-5, 10)]
+Gparas = [2**i for i in range(-10, 3)]
 
 # Execute a subprocess with standard output
 def execute(command, output = False):
@@ -32,13 +33,24 @@ if __name__=="__main__":
     liblinear_test = './svm-predict'
     svm_scale = './svm-scale'
 
+    if len(sys.argv) < 3:
+        print "$train_file $valid_file $scale(set to 1 if use scale) $kernel(use default if not set)"
+        print "kernel: 0--linear, 1--polynomial, 2--rbf, 3--sigmoid, default: [0,2]"
+
     train_file = sys.argv[1]
     valid_file = sys.argv[2]
-    scale = len(sys.argv) >= 4 and sys.argv[3] == '1'
+    scale = len(sys.argv) >= 4 and sys.argv[3] == '1' # whether to scale
+    if len(sys.argv) >= 5:
+        print "using kernel:", kernel_name[sys.argv[4]]
+        svm_kernels = [sys.argv[4]] # set kernel
+    else:
+        print "using default kernel candidate:", kernels
+        svm_kernels = kernels
     final_result_output = '{0}.tune.log'.format(train_file)
     fout = open(final_result_output, 'w')
 
     if scale:
+        print "using scale"
         scale_paras = ' -s train.scale.para {0} > train.scale'.format(train_file)
         execute(svm_scale + scale_paras)
         scale_paras = ' -r train.scale.para {0} > valid.scale'.format(valid_file)
@@ -46,18 +58,27 @@ if __name__=="__main__":
         train_file = 'train.scale'
         valid_file = 'valid.scale'
 
-    for kernel in kernels:
-
+    for kernel in svm_kernels:
         model_file = kernel_name[kernel]+'.model.tune'
-
-        for i in xrange(len(Cparas)):
-            sys.stdout.write('{0} Kernel, C paras:{1} '.format(kernel_name[kernel], Cparas[i]))
-            fout.write('{0} Kernel, C paras:{1} '.format(kernel_name[kernel], Cparas[i]))
-            train_paras = ' -s 0 -t {0} -c {1} -q {2} {3}'.format(kernel, Cparas[i], train_file, model_file)
-            execute(liblinear_train + train_paras)
-            valid_paras = ' {0} {1} valid_predict.output.txt'.format(valid_file, model_file)
-            result = execute(liblinear_test + valid_paras, True)
-            fout.write(result)
+        if kernel == 2: # tune both c and g
+            for c in Cparas:
+                for g in Gparas:
+                    sys.stdout.write('Kernel:{0}, C paras:{1}, Gamma:{2}'.format(kernel_name[kernel], c, g))
+                    fout.write('Kernel:{0}, C paras:{1}, Gamma:{2}'.format(kernel_name[kernel], c, g))
+                    train_paras = ' -s 0 -t {0} -c {1} -g {2} -q {3} {4}'.format(kernel, c, g, train_file, model_file)
+                    execute(liblinear_train + train_paras)
+                    valid_paras = ' {0} {1} valid_predict.output.txt'.format(valid_file, model_file)
+                    result = execute(liblinear_test + valid_paras, True)
+                    fout.write(result)
+        else: # tune c
+            for c in Cparas:
+                sys.stdout.write('Kernel:{0}, C paras:{1} '.format(kernel_name[kernel], c))
+                fout.write('Kernel:{0}, C paras:{1} '.format(kernel_name[kernel], c))
+                train_paras = ' -s 0 -t {0} -c {1} -q {2} {3}'.format(kernel, c, train_file, model_file)
+                execute(liblinear_train + train_paras)
+                valid_paras = ' {0} {1} valid_predict.output.txt'.format(valid_file, model_file)
+                result = execute(liblinear_test + valid_paras, True)
+                fout.write(result)
 
     fout.close()
 
