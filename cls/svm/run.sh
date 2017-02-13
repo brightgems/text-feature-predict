fb54611e336f7acc369e2b73ff9f9fc834a8862f
yiren <irenew713@gmail.com>
2017-02-13 16:50:42 -0600

    run scripts

--- cls/svm/run.sh
+++ cls/svm/run.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 tune_log=$1
-path="/home/yiren/Documents/time-series-predict/data/test/"
  [ -d "results/" ] || mkdir -p "results/"
 
 while IFS= read -r line
 do
-    IFS= read -r feature
-    if [ ! $feature ]
+    IFS= read -r path
+    if [ ! $path ]
     then
         break
     fi
+    IFS='/' read -r -a folders <<< $path
+    folder_feature=${folders[-2]}
+    folder_base=${folders[-1]}
+
     IFS= read -r line
     IFS= read -r line
     IFS= read -r model
@@ -24,19 +27,15 @@ do
     IFS=':' read -r -a g <<< ${params[4]}
     g=${g[1]}
 
-    echo "training for" $feature, kernel:$kernel, c:$c, g:$g
+    echo "training for" $folder_base, kernel:$kernel, c:$c, g:$g
 
-    # todo: may need parsing here
-    upper_folder=$feature
-
-    train_path=$path/$upper_folder/$feature/
-    obj_path="results/"$upper_folder/$feature
+    obj_path="results/"$folder_feature/$folder_base
      [ -d $obj_path ] || mkdir -p $obj_path
 
-    train_file=$train_path/$feature".train"
-    test_file=$train_path/$feature".test"
-    model="$obj_path/model.$feature"
-    output="$obj_path/output.$feature"
+    train_file=$path/$folder_base".train"
+    test_file=$path/$folder_base".test"
+    model="$obj_path/model.$folder_base"
+    output="$obj_path/output.$folder_base"
 
     ./svm-scale -s "$obj_path/train.scale.para" "$train_file" > "$train_file.scaled"
     ./svm-scale -r "$obj_path/train.scale.para" "$test_file" > "$test_file.scaled"
--- cls/svm/run_tune.sh
+++ cls/svm/run_tune.sh
@@ -3,14 +3,22 @@
 kernel=2 # use rbf kernel (for topic modeling)
 # kernel=0 # use linear kernel (for bow/tfidf baseline)
 
-path_in="/home/yiren/Documents/time-series-predict/data/test/"
-echo $path_in
+tune_folder=$1
 
-for path_feature in $(find $path_in* -maxdepth 1 -mindepth 0 -type d);
+path_in="/home/yiren/Documents/time-series-predict/data/test"
+echo $path_in
+for path_feature in $(find $path_in/* -maxdepth 0 -mindepth 0 -type d);
 do
-    for folder in $(find $path_feature* -maxdepth 1 -mindepth 1 -type d);
+    if [ $tune_folder ];
+    then
+        if [ ! $tune_folder = $(basename $path_feature) ];
+        then
+            continue
+        fi
+    fi
+    for folder in $(find $path_feature/* -maxdepth 1 -mindepth 0 -type d);
     do
-        echo "$(basename $folder)";
+        echo $folder;
         train_file=$folder/$(basename $folder)".train"
         valid_file=$folder/$(basename $folder)".valid"
         python tune_params_svm.py $train_file $valid_file 1 $kernel
