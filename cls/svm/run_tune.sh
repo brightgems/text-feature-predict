102e49ef077091e04d2496f7a8451f5fefdb54d5
yiren <irenew713@gmail.com>
2017-02-14 15:32:23 -0600

    add tuning for svr

--- cls/svm/run_tune.sh
+++ cls/svm/run_tune.sh
@@ -3,11 +3,21 @@
 kernel=2 # use rbf kernel (for topic modeling)
 # kernel=0 # use linear kernel (for bow/tfidf baseline)
 
-tune_folder=$1
-IFS=';' read -ra folders <<< $tune_folder
-
-path_in="/home/yiren/Documents/time-series-predict/data/features"
+path_in="/home/yiren/Documents/time-series-predict/data/test"
 echo $path_in
+
+tune_model=$1
+if [ $tune_model = 'svr' ];
+then
+    tune_script="tune_params_svr.py"
+    echo "tuning svr (epsilon-svm)"
+else
+    tune_script="tune_params_svm.py"
+    echo "tuning svm (c-svm)"
+fi
+
+tune_folder=$2
+IFS=';' read -ra folders <<< $tune_folder
 
 for path_feature in $(find $path_in/* -maxdepth 0 -mindepth 0 -type d);
 do
@@ -33,15 +43,15 @@ do
         echo $folder;
         train_file=$folder/$(basename $folder)".train"
         valid_file=$folder/$(basename $folder)".valid"
-        python tune_params_svm.py $train_file $valid_file 1 $kernel
+        python $tune_script $train_file $valid_file 1 $kernel
         rm -f "$train_file.scale.para" "$train_file.scale" "$valid_file.scale" "$valid_file.scale.model" "$valid_file.scale.output"
     done
 done
 
 
-# "topic_change;topic_hist_d0.7_w1;topic_hist_d0.7_w1_cont;topic_hist_d0.7_w2;topic_hist_d0.7_w2_cont;topic_hist_d0.7_w3;topic_hist_d0.7_w3_cont;topic_hist_d0.7_w4;topic_hist_d0.7_w4_cont;topic_hist_d0.7_w5"
-# "topic_hist_d0.7_w5_cont;topic_hist_d0.7_w6;topic_hist_d0.7_w6_cont;topic_hist_d0.8_w1;topic_hist_d0.8_w1_cont;topic_hist_d0.8_w2;topic_hist_d0.8_w2_cont;topic_hist_d0.8_w3;topic_hist_d0.8_w3_cont;topic_hist_d0.8_w4"
-# "topic_hist_d0.8_w4_cont;topic_hist_d0.8_w5;topic_hist_d0.8_w5_cont;topic_hist_d0.8_w6;topic_hist_d0.8_w6_cont;topic_hist_d0.9_w1;topic_hist_d0.9_w1_cont;topic_hist_d0.9_w2;topic_hist_d0.9_w2_cont;topic_hist_d0.9_w3"
-# "topic_hist_d0.9_w3_cont;topic_hist_d0.9_w4;topic_hist_d0.9_w4_cont;topic_hist_d0.9_w5;topic_hist_d0.9_w5_cont;topic_hist_d0.9_w6;topic_hist_d0.9_w6_cont;topic_hist_d1_w1;topic_hist_d1_w1_cont;topic_hist_d1_w2"
-# "topic_hist_d1_w2_cont;topic_hist_d1_w3;topic_hist_d1_w3_cont;topic_hist_d1_w4;topic_hist_d1_w4_cont;topic_hist_d1_w5;topic_hist_d1_w5_cont;topic_hist_d1_w6;topic_hist_d1_w6_cont"
+# ./run_tune.sh "svr" "topic_change;topic_hist_d0.7_w1;topic_hist_d0.7_w1_cont;topic_hist_d0.7_w2;topic_hist_d0.7_w2_cont;topic_hist_d0.7_w3;topic_hist_d0.7_w3_cont;topic_hist_d0.7_w4;topic_hist_d0.7_w4_cont;topic_hist_d0.7_w5" > tune_summary/log.reg.tune.1
+# ./run_tune.sh "svr" "topic_hist_d0.7_w5_cont;topic_hist_d0.7_w6;topic_hist_d0.7_w6_cont;topic_hist_d0.8_w1;topic_hist_d0.8_w1_cont;topic_hist_d0.8_w2;topic_hist_d0.8_w2_cont;topic_hist_d0.8_w3;topic_hist_d0.8_w3_cont;topic_hist_d0.8_w4" > tune_summary/log.reg.tune.2
+# ./run_tune.sh "svr" "topic_hist_d0.8_w4_cont;topic_hist_d0.8_w5;topic_hist_d0.8_w5_cont;topic_hist_d0.8_w6;topic_hist_d0.8_w6_cont;topic_hist_d0.9_w1;topic_hist_d0.9_w1_cont;topic_hist_d0.9_w2;topic_hist_d0.9_w2_cont;topic_hist_d0.9_w3" > tune_summary/log.reg.tune.3
+# ./run_tune.sh "svr" "topic_hist_d0.9_w3_cont;topic_hist_d0.9_w4;topic_hist_d0.9_w4_cont;topic_hist_d0.9_w5;topic_hist_d0.9_w5_cont;topic_hist_d0.9_w6;topic_hist_d0.9_w6_cont;topic_hist_d1_w1;topic_hist_d1_w1_cont;topic_hist_d1_w2" > tune_summary/log.reg.tune.4
+# ./run_tune.sh "svr" "topic_hist_d1_w2_cont;topic_hist_d1_w3;topic_hist_d1_w3_cont;topic_hist_d1_w4;topic_hist_d1_w4_cont;topic_hist_d1_w5;topic_hist_d1_w5_cont;topic_hist_d1_w6;topic_hist_d1_w6_cont" > tune_summary/log.reg.tune.5
 
---
+++ cls/svm/tune_params_svr.py
@@ -0,0 +1,124 @@
+# used to tune parameters for epsilon-support vector regression
+# usage: python TuneParameters train_file valid_file use_scale_or_not
+# Highly recommend scale it
+# tuning c, epsilon, kernel, gamma, shrinking
+
+import os
+import copy
+import time
+import random
+import sys
+import shutil
+import subprocess
+
+from subprocess import STDOUT
+
+kernels = [2]
+kernel_name = ['linear', 'polynomial', 'rbf', 'sigmoid']
+#Cparas = [2**i for i in range(-5, 10)]
+#Gparas = [2**i for i in range(-10, 3)]
+#Epsilon = [0, 0.01, 0.1, 0.5, 1, 2, 4, 8]
+Cparas = [2**i for i in range(-1, 1)]
+Gparas = [2**i for i in range(-1, 1)]
+Epsilon = [0.01, 0.1]
+
+
+# Execute a subprocess with standard output
+def execute(command, output=False):
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    lines_iterator = iter(popen.stdout.readline, b"")
+    returned_result = ''
+    for line in lines_iterator:
+        if output:
+            print line # yield line
+        returned_result += line
+    return returned_result
+
+def parse_result(result):
+    lines = result.split("\n")
+    mse = float(lines[0].split("= ")[-1].split(" (")[0])
+    scc = float(lines[1].split("= ")[-1].split(" (")[0])
+    return mse, scc
+
+if __name__=="__main__":
+    liblinear_train = './svm-train'
+    liblinear_test = './svm-predict'
+    svm_scale = './svm-scale'
+
+    if len(sys.argv) < 3:
+        print "$train_file $valid_file $scale(set to 1 if use scale) $kernel(use default if not set)"
+        print "kernel: 0--linear, 1--polynomial, 2--rbf, 3--sigmoid, default: [0,2]"
+
+    train_file = sys.argv[1]
+    valid_file = sys.argv[2]
+    scale = len(sys.argv) >= 4 and sys.argv[3] == '1' # whether to scale
+    if len(sys.argv) >= 5:
+        print "\tusing kernel:", kernel_name[int(sys.argv[4])]
+        svm_kernels = [int(sys.argv[4])] # set kernel
+    else:
+        print "\tusing default kernel candidate:", kernels
+        svm_kernels = kernels
+
+    try:
+        os.stat("tune_svr_logs")
+    except:
+        os.mkdir("tune_svr_logs")
+    final_result_output = 'tune_svr_logs/{0}.tune.log'.format(train_file.split("/")[-1])
+    fout = open(final_result_output, 'w')
+
+    if scale:
+        print "\tusing scale"
+        scale_paras = ' -s {0}.scale.para {0} > {0}.scale'.format(train_file)
+        execute(svm_scale + scale_paras)
+        scale_paras = ' -r {0}.scale.para {1} > {1}.scale'.format(train_file, valid_file)
+        execute((svm_scale + scale_paras))
+        train_file = '{0}.scale'.format(train_file)
+        valid_file = '{0}.scale'.format(valid_file)
+
+    best_model = {"mse": 100000., "scc": 1., "kernel": -1, "c": -1, "g": -1, 'eps': -1}
+
+    for kernel in svm_kernels:
+        model_file = valid_file + ".model"
+        output_file = valid_file + ".output"
+        if kernel == 2: # tune both c, g and epsilon
+            for c in Cparas:
+                for g in Gparas:
+                    for eps in Epsilon:
+                        # sys.stdout.write('Kernel:{0}, C paras:{1}, Gamma:{2}'.format(kernel_name[kernel], c, g))
+                        fout.write('Kernel:{0}, C:{1}, Gamma:{2}, Eps:{3}\n'
+                                   .format(kernel_name[kernel], c, g, eps))
+                        train_paras = ' -s 3 -t {0} -c {1} -g {2} -p {3} -q {4} {5}'\
+                                      .format(kernel, c, g, eps, train_file, model_file)
+                        execute(liblinear_train + train_paras)
+                        valid_paras = ' {0} {1} {2}'.format(valid_file, model_file, output_file)
+                        result = execute(liblinear_test + valid_paras, False)
+                        fout.write(result+"\n")
+                        mse, scc = parse_result(result)
+                        if mse < best_model["mse"]:
+                            best_model["mse"] = mse
+                            best_model["scc"] = scc
+                            best_model["kernel"] = kernel
+                            best_model["c"] = c
+                            best_model["g"] = g
+                            best_model["eps"] = eps
+        else: # tune c and epsilon
+            for c in Cparas:
+                for eps in Epsilon:
+                    # sys.stdout.write('Kernel:{0}, C paras:{1} '.format(kernel_name[kernel], c))
+                    fout.write('Kernel:{0}, C:{1}, Eps:{2}\n'.format(kernel_name[kernel], c, eps))
+                    train_paras = ' -s 3 -t {0} -c {1} -p {2} -q {3} {4}'.format(kernel, c, eps, train_file, model_file)
+                    execute(liblinear_train + train_paras)
+                    valid_paras = ' {0} {1} {2}'.format(valid_file, model_file, output_file)
+                    result = execute(liblinear_test + valid_paras, False)
+                    fout.write(result+"\n")
+                    mse, scc = parse_result(result)
+                    if mse < best_model["mse"]:
+                        best_model["mse"] = mse
+                        best_model["scc"] = scc
+                        best_model["kernel"] = kernel
+                        best_model["c"] = c
+                        best_model["eps"] = eps
+    print "\tBest model: Kernel:{0}, C:{1}, Gamma:{2}, Eps:{3}, MSE:{4}, SCC:{5}\n"\
+           .format(best_model["kernel"], best_model["c"], best_model["g"], best_model["eps"],
+                   best_model["mse"], best_model["scc"])
+    fout.close()
