9b969ce302a2f5f95600648a4b269a45d9907698
dseyler2 <dseyler2@illinois.edu>
2017-04-14 15:30:55 -0500

    added option to perform a balanced train/test/val split

--- pre-process/data_split.py
+++ pre-process/data_split.py
@@ -1,8 +1,9 @@
 import random
+import numpy as np
 from datetime import date
 from data_separate import to_date
 
-def separate_train_test_validation(split_date, perc_val, f_label, f_split):
+def separate_train_test_validation(split_date, perc_val, f_label, f_split, balance=False):
     '''
     Format:
     Company, Date, Id, Open, Close, Label, Dataset
@@ -36,24 +37,24 @@ def separate_train_test_validation(split_date, perc_val, f_label, f_split):
     for i in range(int(len(train) * perc_val)):
         validation.append(train.pop())
 
-    num_train = 0
-    num_test = 0
-    num_val = 0
+    if balance:
+        train = balance_dataset_binary(train)
+        test = balance_dataset_binary(test)
+        if len(validation) > 0:
+            validation = balance_dataset_binary(validation)
 
-    for line in lines:
-        data_set_id = -1
+    num_train = len(train)
+    num_test = len(test)
+    num_val = len(validation)
 
-        if line in train:
-            data_set_id = 0
-            num_train += 1
-        elif line in test:
-            data_set_id = 1
-            num_test += 1
-        elif line in validation:
-            data_set_id = 2
-            num_val += 1
+    for line in train:
+        fout.write("{},{}\n".format(line.strip(), 0))
 
-        fout.write(line.strip() + ",{}\n".format(str(data_set_id)))
+    for line in test:
+        fout.write("{},{}\n".format(line.strip(), 1))
+
+    for line in validation:
+        fout.write("{},{}\n".format(line.strip(), 2))
 
     total = 0.0 + num_val + num_test + num_train
 
@@ -61,10 +62,36 @@ def separate_train_test_validation(split_date, perc_val, f_label, f_split):
         .format(num_train / total, num_test / total, num_val / total)
 
 
+def split_by_label(dataset):
+    label_split = dict()
+
+    for line in dataset:
+        label = int(line.strip().split(',')[-1])
+        if label not in label_split:
+            label_split[label] = []
+        label_split[label].append(line)
+
+    return label_split
+
+
+def balance_dataset_binary(dataset):
+    label_split = split_by_label(dataset)
+    if len(label_split[0]) > len(label_split[1]):
+        label_split[1].extend(np.random.choice(label_split[1], len(label_split[0]) - len(label_split[1])))
+    else:
+        label_split[0].extend(np.random.choice(label_split[0], len(label_split[1]) - len(label_split[0])))
+
+    print len(label_split[0]), len(label_split[1])
+
+    label_split[0].extend(label_split[1])
+    return label_split[0]
+
+
+
 if __name__ == '__main__':
     data_path = "/Users/ds/git/financial-topic-modeling/data/bpcorpus/"
     f_labels = data_path + "corpus_labels.csv"
-    f_split = data_path + "corpus_lablels_split.csv"
+    f_split = data_path + "corpus_labels_split.csv"
     split_date = date(2012, 5, 15)
 
-    separate_train_test_validation(split_date, 0.0, f_labels, f_split)
+    separate_train_test_validation(split_date, 0.0, f_labels, f_split, True)
